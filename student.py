841dd8ddc3b9402c358d986fd557fb77effa92b3
CHCode02 <88426426+CHCode02@users.noreply.github.com>
2023-09-16 17:08:25 +0800

    Add files via upload

--- student.py
+++ student.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
+from botocore.exceptions import ClientError
 from config import *
 
 app = Flask(__name__)
@@ -20,9 +21,13 @@ db_conn = connections.Connection(
 )
 output = {}
 table = 'student'
-
+s3=boto3.client('s3')
 
 #if call / then will redirect to that pg
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('Home.html')
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -52,6 +57,8 @@ def studentReg():
 
         cursor.execute(insert_sql, (cohort,intern_period,student_name,student_id,student_ic,gender,programme,student_email,student_contact,uni_supervisor_name,uni_supervisor_email))
         db_conn.commit()
+
+
         
 
     except Exception as e:
@@ -62,7 +69,7 @@ def studentReg():
         cursor.close()
 
     print("all modification done...")
-    return render_template('StudentRegister.html', registerSuccessful=True)
+    return render_template('StudRegister.html', registerSuccessful=True)
 
 
 
